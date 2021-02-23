b931606b29220469a7f64ee7fbdbbd858687d6f1
Jonathan Dilks <git@jonathandilks.com>
2021-02-23 19:01:27 +0000

    New file structure

---
+++ routes/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+// and set the environment variables. See http://twil.io/secure
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.send('It works!');
+});
+
+router.get('/sendSMS', (req, res) => {
+
+    client.messages
+    .create({
+       body: 'McAvoy or Stewart? These timelines can get so confusing.',
+       from: '+1 207 337 9878',
+      //  statusCallback: 'https://abc1234.free.beeceptor.com',
+       to: '+447766952599'
+     })
+    .then(message => console.log(message.sid));
+
+    res.send('SMS sent');
+  });
+
+module.exports = router;
