d68569d21168badd732543993e1751bce9bbfec8
Jonathan Dilks <git@jonathandilks.com>
2021-02-25 18:21:35 +0000

    Tidying

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,7 @@
 const express = require("express");
 
 const router = express.Router();
-
-// router.get("/", (req, res) => {
-//   res.render("home");
-// });
-
 router.post("/sendSMS", require("./sendSMS"));
-
 router.post("/statusCallback", require("./statusCallback"));
 
 module.exports = router;
--- routes/statusCallback.js
+++ routes/statusCallback.js
@@ -1,17 +1,19 @@
-const io = require('socket.io')(3001, {
-    cors: {
-      origin: "*",
-      methods: ["GET", "POST"]
-    }
-  }
-  )
-  
+const io = require("socket.io")(3001, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`socket.io connection received. Host:`, socket.handshake.headers.host);
+});
 
 module.exports = (req, res) => {
   const { MessageSid, MessageStatus } = req.body;
 
   console.log(`SID: ${MessageSid}, Status: ${MessageStatus}`);
-  io.emit('SmsStatusUpdate', { MessageSid, MessageStatus });
+  io.emit("SmsStatusUpdate", { MessageSid, MessageStatus });
 
   res.sendStatus(200);
 };
